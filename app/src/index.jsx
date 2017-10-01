fd031044504659448d1ebf8fb83df61360141370
Tatiana Tun <tatiana.tun@nytimes.com>
2017-10-01 00:54:06 -0400

    Improve eslint config

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,7 +1,7 @@
 /* global document */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './components/App/App';
 
 ReactDOM.render((
